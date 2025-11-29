32255958ea19d7fe493064554400bf7959b68f11
Ege Doğukan Seven <72814782+EgeDogukan@users.noreply.github.com>
2025-11-29 21:20:32 +0300

    feat(domain): add core enums and value objects
    - defined ZoneType, SliceType and RewarType enums
    - created immutable value objects: Reward, Slice, WheelDefinition, ZoneState

---
+++ Assets/Scripts/Domain/CoreTypes.cs
@@ -0,0 +1,81 @@
+#nullable enable
+using System.Collections.Generic;
+
+namespace WheelGame.Domain
+{
+    // enums
+    public enum ZoneType
+    {
+        Normal,     // Normal one, contains bomb
+        SafeSilver, // no bomb, standart rewrds every 5th
+        SuperGold   // no bomb, big rewards every 30th
+    }
+
+    public enum SliceType
+    {
+        Reward,
+        Bomb
+    }
+
+    public enum RewardType
+    {
+        Cash,
+        Gold,
+        Ticket
+        // TODO: extend it later based on the rewards, need to think first if im gonna count knife and a gun both weapon, we will see
+    }
+
+    // we build slices from rewards, then build the wheel from both zonestates and slices
+
+    public sealed class ZoneState
+    {
+        public int Index { get; }
+        public ZoneType ZoneType { get; }
+
+        public ZoneState(int index, ZoneType zoneType)
+        {
+            Index = index;
+            ZoneType = zoneType;
+        }
+    }
+
+    public sealed class Reward
+    {
+        public RewardType Type { get; }
+        public int Amount { get; }
+
+        public Reward(RewardType type, int amount)
+        {
+            Type = type;
+            Amount = amount;
+        }
+    }
+
+    public sealed class Slice
+    {
+        public SliceType SliceType { get;}
+        public Reward? Reward { get; }
+    
+        
+        public bool IsBomb => SliceType == SliceType.Bomb;
+
+        public Slice(SliceType sliceType, Reward? reward)
+        {
+            SliceType = sliceType;
+            Reward = reward;
+            
+        }
+    }
+
+    public sealed class WheelDefinition
+    {
+        public ZoneType ZoneType { get; }
+        public IReadOnlyList<Slice> Slices { get; }
+
+        public WheelDefinition(ZoneType zoneType, IReadOnlyList<Slice> slices)
+        {
+            ZoneType = zoneType;
+            Slices = slices;
+        }
+    }
+}
